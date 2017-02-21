0539984d8799ad2bc9ec00eb53d74a513ffccbac
abaojin <abaojin@gmail.com>
2017-02-21 19:28:23 +0800

    lua make
    Signed-off-by: abaojin <abaojin@gmail.com>

---
+++ Lua.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/***
+ * Lua.cs
+ * 
+ * @author abaojin
+ * 
+ * lua 启动程序入口，目前可以直接传入lua文件的路径
+ */
+namespace CsharpLua
+{
+    public class Lua
+    {
+        private static string ScriptFile;
+
+        static void Main(string[] args)
+        {
+            // 初始化lua状态机
+            ILuaState lstate = new LuaState();
+            // 加载lua库
+            lstate.L_OpenLibs();
+
+            if(args.Length > 0) {
+                ScriptFile = args[0];
+            } else {
+                throw new Exception("args is null");
+            }
+
+            // 执行lua脚本
+            ThreadStatus status = lstate.L_DoFile(ScriptFile);
+            if (status != ThreadStatus.LUA_OK) {
+                throw new Exception(lstate.ToString(-1));
+            }
+        }
+    }
+}
--- bin/Debug/test.lua
+++ bin/Debug/test.lua
@@ -1,1 +1,1 @@
-print("hello")
+string.upper("hello")
